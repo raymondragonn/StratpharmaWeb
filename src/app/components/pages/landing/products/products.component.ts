1f242a25ab8a795eb2b9411b295a7d9db81c474d
Ray Mondragon <raymondragoon@outlook.com>
2026-03-26 13:40:19 -0600

    FIX Descripciones de productos en página principal.

--- src/app/components/pages/landing/products/products.component.ts
+++ src/app/components/pages/landing/products/products.component.ts
@@ -23,27 +23,27 @@ export class ProductsComponent {
       id: '1',
       title: 'Strata-triz',
       description:
-        'Gel de silicona para el tratamiento de cicatrices nuevas y antiguas, así como para la prevención de cicatrices anormales.',
+        'Gel de silicona para el tratamiento de cicatrices nuevas y antiguas.',
       imageUrl: 'assets/img/products/strata-triz/imgi_44_ST-5-ES-Box-HighRes.jpg'
     },
     {
       id: '4',
       title: 'Stratamark',
-      description: 'Gel formulado para la prevención y el tratamiento de las estrías.',
+      description: 'Gel de silicona formulado para la prevención y el tratamiento de las estrías.',
       imageUrl: 'assets/img/products/stratamark/imgi_43_SK20EX00TubeBoxLowRes1.jpg'
     },
     {
       id: '2',
       title: 'Stratamed',
       description:
-        'Apósito en gel para una cicatrización más rápida en heridas abiertas o recién suturadas.',
+        'Apósito en gel de silicona para la protección y cicatrización más rápida en heridas abiertas o recién suturadas.',
       imageUrl: 'assets/img/products/stratamed/imgi_104_MS20EX00TubeBoxLowRes.jpg'
     },
     {
       id: '3',
       title: 'Stratacel',
       description:
-        'Apósito en gel diseñado para procedimientos fraccionados y tratamientos post-laser.',
+        'Apósito en gel diseñado para recuperar la piel tras procedimientos fraccionados y tratamientos post-laser.',
       imageUrl: 'assets/img/products/stratacel/imgi_73_SC20EX00BoxTubeHighRes-1.jpg'
     },
     {
